c0681d2abf676793909fabd88e465c3a115d673d
nastae <saauriscom>
2019-07-21 20:06:03 +0300

    Add player's bank

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 ﻿import deck as d
 from play import Player, Dealer
-import display
 import os
 
 def main():
     menu()
+
+# https://runnable.com/docker/python/dockerize-your-python-application - dockerize
+# https://bicyclecards.com/how-to-play/blackjack/ rules
 
 def menu():
     print('Welcome to terminal blackjack!')
@@ -20,29 +22,53 @@ def menu():
 
 def startGame():
     name = input('Write your name: ')
+    dealer = Dealer('dealer', 0.0)
+    player = Player(name, 100.0)
     option = ''
     while option != 'q':
-        game(name)
+        game(name, dealer, player)
         option = input('Press Enter to play again or type a "q" to end the program: ')
 
-def game(name):
+def game(name, dealer, player):
     print('A new game started!')
     deck = d.getShuffledDeck()
-    dealer = Dealer('dealer')
-    player = Player(name)
-    dealer.hit(deck)
-    player.hit(deck)
-    player.hit(deck)
-    display.print_hands([dealer, player])
+    player.print_bank()
+    bet = input('Type your bet: ')
+    while not (bet.isdigit() and float(bet) > 0 and float(bet) <= player.getBank()):
+        print('Wrong bet value!')
+        bet = input('Type your bet: ')
+    player.minusBank(int(bet))
+    dealer.getHands()[0].hit(deck)
+    if dealer.getHands()[0].getHand()[0] in ['A', '10']:
+        dealer.getHands()[0].hit(deck)
+    player.getHands()[0].hit(deck)
+    player.getHands()[0].hit(deck)
+    for p in [dealer, player]:
+        p.print_hand()
     print(name, 'turn!')
-    playerHandValue = player.play(deck, display)
+    playerHandValue = player.play(deck)
     if playerHandValue <= 21:
-        dealerHandValue = dealer.play(deck, display)
+        if playerHandValue == 21:
+            print('Player gives 1.5x his bet!')
+            player.addBank(int(bet) * 1.5)
+        dealerHandValue = dealer.play(deck)
         if dealerHandValue <= 21:
-            if playerHandValue >= dealerHandValue:
+            if playerHandValue > dealerHandValue:
                 print('{sep}\nYou won!\n{sep}\n'.format(sep='-' * 16))
+                player.addBank(int(bet) * 2)
+            elif playerHandValue == 21 or playerHandValue == dealerHandValue:
+                print('{sep}\nDraw! You gave your bet back!\n{sep}\n'.format(sep='-' * 16))
+                player.addBank(int(bet))
             else:
                 print('{sep}\nYou lost!\n{sep}\n'.format(sep='-' * 16))
+        else:
+            print('{sep}\nYou won!\n{sep}\n'.format(sep='-' * 16))
+            player.addBank(int(bet) * 2)
+    else:
+        print('{sep}\nYou lost!\n{sep}\n'.format(sep='-' * 16))
+    player.print_bank()
+    player.clear()
+    dealer.clear()
 
 if __name__ == '__main__':
     main()
--- src/play.py
+++ src/play.py
@@ -1,20 +1,142 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.hand = []
+    
+    def __init__(self, name, bank):
+        self.__name = name
+        self.__bank = bank
+        self.__hands = []
+        self.__hands.append(Hand(self.__name))
+    
+    def splitHand(self):
+        del self.__hands[0].getHand()[1]
+        self.__hands.append(Hand(self.__name))
+        self.__hands[1].getHand().append(self.__hands[0].getHand()[0])
+
+    def play(self, deck):
+        option = ''
+        if self.__isDoublingDown():
+            self.print_hand()
+            option = self.__chooseDoublingDown()
+            if option == 'y':
+                print('You doubled your bet!')
+                self.print_hand()
+
+        if self.__isSplittingPairs():
+            self.print_hand()
+            option = self.__chooseSplitting()
+            if option == 'y':
+                self.splitHand()
+                self.print_hand()
+
+        for hand in self.__hands:
+            option = self.__choosePlay()
+            value = hand.countValue()
+            while option != 's' and value <= 21:
+                if option == 'h':
+                    hand.hit(deck)
+                    self.print_hand()
+                value = hand.countValue()
+                if value <= 21:
+                    option = self.__choosePlay()
+        return self.__getMaxHandValue()
+
+    def __getMaxHandValue(self):
+        max_value = self.__hands[0].countValue()
+        for hand in self.__hands:
+            temp = hand.countValue()
+            if (temp > max_value and temp <= 21) or (max_value > 21 and temp <= 21):
+                max_value = temp
+        return max_value
+
+    def __choosePlay(self):
+        choosing_text = 'Choose a play, type whether "s" to stand or "h" (hit) to ask for another card: '
+        option = input(choosing_text)
+        while option not in ['h', 's']:
+            print('The chosen key is incorrect. Try again!')
+            option = input(choosing_text)
+        return option
+
+    def __chooseSplitting(self):
+        choosing_text = 'Type "y" to split your hand or "n" to cancel: '
+        option = input(choosing_text)
+        while option not in ['y', 'n']:
+            print('The chosen key is incorrect. Try again!')
+            option = input(choosing_text)
+        return option
+
+    def __chooseDoublingDown(self):
+        choosing_text = 'Type "y" to double your bet or "n" to cancel: '
+        option = input(choosing_text)
+        while option not in ['y', 'n']:
+            print('The chosen key is incorrect. Try again!')
+            option = input(choosing_text)
+        return option
+
+    def __isSplittingPairs(self):
+        return len(self.__hands[0].getHand()) == 2 and self.__hands[0].getHand()[0] == self.__hands[0].getHand()[1]
+
+    def __isDoublingDown(self):
+        return len(self.__hands[0].getHand()) == 2 and self.__hands[0].countValue() in [9, 10, 11]
+
+    def getHands(self):
+        return self.__hands
+
+    def getName(self):
+         return self.__name
+
+    def getBank(self):
+        return self.__bank
+
+    def minusBank(self, minus):
+        self.__bank = self.__bank - minus
+    
+    def addBank(self, addition):
+        self.__bank = self.__bank + addition
+
+    def print_bank(self):
+        print("Player: {name}\nBank: {bank}".format(
+            name=self.__name,
+            bank=self.__bank))
+
+    def print_hand(self):
+        print("{sep}\n{name}\n{hand}\n{value}\n{sep}\n".format(
+                sep='-' * 16, 
+                name='Dealer' if isinstance(self, Dealer) else 'Player: ', 
+                hand=self.__getHand(),
+                value=self.__getHandValue()))
+
+    def __getHand(self):
+            if len(self.getHands()) == 1:
+                    return "Hand: " + " ".join(map(str, self.getHands()[0].getHand()))
+            else:
+                    return 'Right hand: ' + " ".join(map(str, self.getHands()[0].getHand())) + ', left hand: ' + " ".join(map(str, self.getHands()[1].getHand()))
+
+    def __getHandValue(self):
+            if len(self.getHands()) == 1:
+                    return "Hand value: " + str(self.getHands()[0].countValue())
+            else:
+                    return "Right hand value: " + str(self.getHands()[0].countValue()) + ", left hand value: " + str(self.getHands()[1].countValue())
+
+    def clear(self):
+        self.__hands = []
+        self.__hands.append(Hand(self.__name))
+
+class Hand:
+    def __init__(self, owner):
+        self.__owner = owner
+        self.__hand = []
     
     def hit(self, deck):
         if len(deck) == 0:
             raise Exception('Deck is empty!')
         card = deck[0]
-        print(self.name, 'gave', card, '!')
-        self.hand.append(card)
+        print(self.__owner, 'gave', card, '!')
+        self.__hand.append(card)
         del deck[0]
         return card
 
-    def countHandValue(self):
+    def countValue(self):
         value = 0
-        for card in self.hand:
+        for card in self.__hand:
             if card.isdigit():
                 value += int(card)
             if card in ['B', 'Q', 'K']:
@@ -23,7 +145,7 @@ class Player:
         return value
 
     def countAcesValue(self, valueWithoutAces):
-        aceCount = self.hand.count('A')
+        aceCount = self.__hand.count('A')
         if aceCount == 1:
             if valueWithoutAces + 11 > 21:
                 return 1
@@ -46,45 +168,14 @@ class Player:
                 return 14
         return 0
 
-    def play(self, deck, display):
-        option = self.__choosePlay()
-        value = self.countHandValue()
-        while option != 's' and value <= 21:
-            if option == 'h':
-                self.hit(deck)
-                display.print_hands([self])
-            value = self.countHandValue()
-            if value > 21:
-                print("{sep}\nTotal value of your hand is {value}!\nYou lost!\n{sep}\n".format(
-                    sep='-' * 16,
-                    value=value))
-            else:
-                option = self.__choosePlay()
-        return value
-
-    def __choosePlay(self):
-        choosing_text = 'Choose a play, type whether "s" to stand or "h" (hit) to ask for another card: '
-        option = input(choosing_text)
-        while option not in ['h', 's']:
-            print('The chosen key is incorrect. Try again!')
-            option = input(choosing_text)
-        return option
-
     def getHand(self):
-        return self.hand
-
-    def getName(self):
-         return self.name
+        return self.__hand
 
 class Dealer(Player):
-    def play(self, deck, display):
-        value = self.countHandValue()
+    def play(self, deck):
+        value = self.getHands()[0].countValue()
         while value < 17:
-            self.hit(deck)
-            value = self.countHandValue()
-            if value > 21:
-                print("{sep}\nTotal value of dealer's hand is {value}!\nYou won!\n{sep}\n".format(
-                    sep='-' * 16,
-                    value=value))
-        display.print_hands([self])
+            self.getHands()[0].hit(deck)
+            value = self.getHands()[0].countValue()
+        self.print_hand()
         return value
